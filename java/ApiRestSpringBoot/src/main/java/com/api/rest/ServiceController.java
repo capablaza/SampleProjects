238b1da2034961eb2fbcdac48d9ebc14161c56e3
Carlos <c.apablaza.z@gmail.com>
2016-09-22 15:58:36 -0300

    Ejemplo Responder json utilizando SpringBoot

---
+++ java/ApiRestSpringBoot/src/main/java/com/api/rest/ServiceController.java
@@ -0,0 +1,22 @@
+package com.api.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class ServiceController {
+
+    @RequestMapping("/user")
+    public User getAll(){
+        return new User("carlos apablaza","carlos@email.com");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseBody
+    ErrorInfo handleBadRequest(HttpServletRequest req, Exception ex){
+        return new ErrorInfo(req.getRequestURI(), ex);
+    }
+}
